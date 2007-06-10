9efae9d6d89464989a3cd22989681457d03cb08b
pstradomski <pstradomski@7a9b365d-d22d-0410-8af7-4f32981120b0>
2007-06-10 20:15:41 +0000

    Test osiągalności (tylko dla sieci ograniczonych, dla nieograniczonych nie da się testować).

--- src/petrieditor/modules/treemodule/TreeModuleResultPane.java
+++ src/petrieditor/modules/treemodule/TreeModuleResultPane.java
@@ -1,9 +1,15 @@
 package petrieditor.modules.treemodule;
+
 import java.awt.BorderLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
+import javax.swing.JButton;
 import javax.swing.JEditorPane;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 
 import petrieditor.model.Transition;
@@ -11,7 +17,7 @@ import petrieditor.modules.ResultPane;
 
 public class TreeModuleResultPane extends ResultPane {
     private TreeModuleResultSet resultSet;
-    
+
     public TreeModuleResultPane(TreeModuleResultSet resultSet) {
         super();
         this.resultSet = resultSet;
@@ -25,66 +31,108 @@ public class TreeModuleResultPane extends ResultPane {
         results.setContentType("text/html");
         results.setText(getResultsText());
         add(new JScrollPane(results), BorderLayout.CENTER);
+        if (resultSet.isBounded && !resultSet.rootVertex.getMarking().isEmpty()) {
+            JButton testReachability = new JButton("Test reachability");
+            
+            testReachability.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    String testedMarkingString = JOptionPane.showInputDialog(TreeModuleResultPane.this, "Enter " + TreeModuleResultPane.this.resultSet.rootVertex.getMarking().size() + " comma delimited integers", "Reachability test", JOptionPane.QUESTION_MESSAGE);
+                    if (testedMarkingString == null) {
+                        return;
+                    }
+                    try {
+                        String[] parts = testedMarkingString.split("\\s*,\\s*");
+                        ArrayList<Integer> testedMarking = new ArrayList<Integer>();
+                        for (int i = 0; i < parts.length; i++) {
+                            testedMarking.add(Integer.parseInt(parts[i]));
+                        }
+                        if (testedMarking.size() != TreeModuleResultPane.this.resultSet.rootVertex.getMarking().size()) {
+                            JOptionPane.showMessageDialog(TreeModuleResultPane.this, "Invalid number of items entered", "Error", JOptionPane.ERROR_MESSAGE);
+                            return;
+                        }
+                        if (TreeModuleResultPane.this.resultSet.vertices.keySet().contains(testedMarking)) {
+                            JOptionPane.showMessageDialog(TreeModuleResultPane.this, "This marking is reachable", "Reachability test", JOptionPane.INFORMATION_MESSAGE);
+                        } else {
+                            JOptionPane.showMessageDialog(TreeModuleResultPane.this, "This marking is NOT reachable", "Reachability test", JOptionPane.INFORMATION_MESSAGE);
+                        }
+                    } catch (NumberFormatException nfe) {
+                        JOptionPane.showMessageDialog(TreeModuleResultPane.this, "Invalid numeric format", "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                } 
+            });
+
+            add(testReachability, BorderLayout.SOUTH);
+            
+        }
     }
-    
+
     String getResultsText() {
         String text = "<html><head><title>Execution tree and graph analysis</title></head><body>";
-        
-        text += "<p>The net is " +(resultSet.isBounded ? getGreenText("bounded"): getRedText("NOT bounded"))+ "</p>";
-        
+
+        text += "<p>The net is "
+                + (resultSet.isBounded ? getGreenText("bounded")
+                        : getRedText("NOT bounded")) + "</p>";
+
         if (resultSet.stronglyConnectedComponentsCount > 0) {
-            text += "<p>Number of strongly connected components in execution graph: " +(resultSet.stronglyConnectedComponentsCount)+ "</p>";
-            
+            text += "<p>Number of strongly connected components in execution graph: "
+                    + (resultSet.stronglyConnectedComponentsCount) + "</p>";
+
             if (resultSet.stronglyConnectedComponentsCount > 1) {
-                text += "<p>There are more than one strongly connected components in execution graph; this means the net "+ getRedText("is not reversible")+"</p>";
+                text += "<p>There are more than one strongly connected components in execution graph; this means the net "
+                        + getRedText("is not reversible") + "</p>";
             } else {
-                text += "<p>There is exactly one strongly connected component in execution graph; this means the net " + getGreenText("is reversible")+"</p>";
-            }    
-        }
-        
-        if (resultSet.deadlockFound) {
-            text += "<p>"+getRedText("Deadlock in the network found")+"</p>";
-        } else if (resultSet.isBounded){
-            text += "<p>"+getGreenText("No deadlocks in the network found. As it is bounded this means it is deadlock-free")+"</p>";
-        } else {
-            text += "<p>No deadlocks in the network found. As it is not bounded this means it " + getGreenText("MIGHT BE deadlock-free") + "</p>";
+                text += "<p>There is exactly one strongly connected component in execution graph; this means the net "
+                        + getGreenText("is reversible") + "</p>";
+            }
         }
 
-        
+        if (resultSet.deadlockFound) {
+            text += "<p>" + getRedText("Deadlock in the network found")
+                    + "</p>";
+        } else if (resultSet.isBounded) {
+            text += "<p>"
+                    + getGreenText("No deadlocks in the network found. As it is bounded this means it is deadlock-free")
+                    + "</p>";
+        } else {
+            text += "<p>No deadlocks in the network found. As it is not bounded this means it "
+                    + getGreenText("MIGHT BE deadlock-free") + "</p>";
+        }
+
         text += recursiveOutput(resultSet.rootVertex);
         System.out.println(recursiveOutput(resultSet.rootVertex));
         text += "</body></html>";
-        
+
         return text;
     }
-    
+
     String getGreenText(String text) {
-        return "<span style='color:green; font-weight:bold'>"+text+"</span>";
+        return "<span style='color:green; font-weight:bold'>" + text
+                + "</span>";
     }
-    
+
     String getRedText(String text) {
-        return "<span style='color:red; font-weight:bold'>"+text+"</span>";
+        return "<span style='color:red; font-weight:bold'>" + text + "</span>";
     }
-    
+
     String recursiveOutput(GraphVertex vertex) {
         return recursiveOutput(vertex, null);
     }
-    
+
     String recursiveOutput(GraphVertex vertex, String reachedBy) {
-        String text = "<li>"+vertex;
+        String text = "<li>" + vertex;
         if (reachedBy != null) {
-            text += "(by " +reachedBy + ")";
+            text += "(by " + reachedBy + ")";
         }
         if (!vertex.getExitTransitions().isEmpty()) {
             text += "<ul>";
             for (Transition t : vertex.getExitTransitions()) {
-                text+=recursiveOutput(vertex.getExit(t), t.getName());
-            } 
+                text += recursiveOutput(vertex.getExit(t), t.getName());
+            }
             text += "</ul>";
         }
-        
+
         text += "</li>";
-        
+
         return text;
     }
 }
